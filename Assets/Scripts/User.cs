82119983758d74c083eec49e0244ff44c0b1f05f
Pavel Bu <puchik006@mail.ru>
2024-02-05 23:02:13 +0200

    UsersFactory

---
+++ Assets/Scripts/User.cs
@@ -0,0 +1,17 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class User: MonoBehaviour
+{
+    private TMP_Text _userPhone;
+
+    private void OnValidate()
+    {
+        _userPhone = gameObject.GetComponentInChildren<TMP_Text>();
+    }
+
+    public void Initialize(string phoneNumber)
+    {
+        _userPhone.text = phoneNumber;
+    }
+}
---
+++ Assets/Scripts/UsersFactory.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class UsersFactory: MonoBehaviour
+{
+    [SerializeField] private User _userPrefab;
+    [SerializeField] private RectTransform _usersList;
+
+    public User GetUser(string phoneNumber)
+    {
+        User instance = Instantiate(_userPrefab,_usersList);
+        instance.Initialize(phoneNumber);
+        return instance;
+    }
+}
